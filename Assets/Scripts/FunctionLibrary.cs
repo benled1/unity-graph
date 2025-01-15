a561b1b79de1fbf398f38cfc5b65bada0a759e67
Ben Ledingham <benledingham1@gmail.com>
2025-01-14 23:40:25 -0800

    third dimension to the graph

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -6,21 +6,37 @@ using UnityEngine.Rendering;
 
 public static class FunctionLibrary
 {
-    public static float Wave(float x, float t)
+    public delegate float Function(float x, float z, float t);
+
+    public enum FunctionName {Wave, MultiWave, Ripple};
+    private static Function[] functions = {Wave, MultiWave, Ripple};
+
+    public static Function getFunction(FunctionName name)
+    {
+        return functions[(int)name];
+    }
+    public static float Wave(float x, float z, float t)
     {
         return Mathf.Sin(Mathf.PI * (x + t));
     }
 
-    public static float MultiWave(float x, float t)
+    public static float MultiWave(float x, float z, float t)
     {
         float y = Mathf.Sin(Mathf.PI * (x + t));
         y += Mathf.Sin(2f * Mathf.PI * (x+t))*0.5f;
         return y*(2f/3f);
     }
 
-    public static float Ripple (float x, float t) {
+    public static float Ripple (float x, float z, float t) {
 		float d = Mathf.Abs(x);
         float y = Mathf.Sin(Mathf.PI * (4f * d - t));
 		return y / (1f + 10f * d);
 	}
+
+    public static float MultiWave2(float x, float z, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(4f * Mathf.PI * (x+t))*0.5f;
+        return y*(2f/3f);
+    }
 }
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -9,51 +9,52 @@ public class Graph : MonoBehaviour
     Transform pointPrefab;
     [SerializeField, Range(10,200)]
     int resolution = 10;
-    [SerializeField, Range(0,4)]
-    int function;
-    float[] domain = {-1, 1};
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+    float[] domain = {-3, 3};
     float baseUnit => Math.Abs(domain[1]-domain[0])/resolution;
     float pointShift => baseUnit*0.5f;
-    Transform[] points;
+    Transform[,] points;
+
 
     void Awake()
     {
         float x = domain[0];
+        float z = domain[0];
+
         Vector3 scale = Vector3.one * baseUnit;
-        points = new Transform[resolution];
+        points = new Transform[resolution, resolution];
 
         for (int i=0; i<resolution; i++)
         {
-            Transform currPoint = Instantiate<Transform>(pointPrefab);
-            currPoint.SetParent(this.transform, false);
-            currPoint.localScale = scale;
-            Vector3 oldPosition = currPoint.localPosition;
-            oldPosition.x = x + pointShift;
-            currPoint.localPosition = oldPosition;
-            x = x + baseUnit;
-            points[i] = currPoint;
+            float zShift = baseUnit * i;
+            for (int j=0; j<resolution; j++)
+            {   
+                Transform currPoint = Instantiate<Transform>(pointPrefab);
+                currPoint.SetParent(this.transform, false);
+                currPoint.localScale = scale;
+                Vector3 oldPosition = currPoint.localPosition;
+                oldPosition.x = x + (baseUnit*j) + pointShift;
+                oldPosition.z = z + zShift;
+                currPoint.localPosition = oldPosition;
+                points[i,j] = currPoint;
+            }
         }
     }
 
-
     void Update() 
     {
-        for (int i=0;i<points.Length;i++)
+        FunctionLibrary.Function f = FunctionLibrary.getFunction(function);
+        Debug.Log(points.GetLength(0));
+        for (int i=0;i<points.GetLength(0);i++)
         {
-            Transform currPoint = points[i];
-            Vector3 oldPosition = currPoint.localPosition;
-            if (function == 0)
+            for (int j=0;j<points.GetLength(1);j++)
             {
-                oldPosition.y = FunctionLibrary.Wave(currPoint.position.x, Time.time);
-            }else if (function == 1)
-            {
-                oldPosition.y = FunctionLibrary.MultiWave(currPoint.position.x, Time.time);
-            }else if (function == 2)
-            {
-                oldPosition.y = FunctionLibrary.Ripple(currPoint.position.x, Time.time);
+                Transform currPoint = points[i,j];
+                Vector3 oldPosition = currPoint.localPosition;
+                oldPosition.y = f(currPoint.position.x, currPoint.position.z, Time.time);
+                currPoint.localPosition = oldPosition;
             }
-            currPoint.localPosition = oldPosition;
-
         }
     }
 }
